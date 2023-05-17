df898ced87ef2f87719d47b96e0c94b4215117e8
JesusChavez-RinoRisk <desarrollo4@rinorisk.com>
2023-05-17 06:44:18 -0600

    Avances

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -8,16 +8,8 @@ import routesDefault from './routes/routesDefault'
 import useJwt             from '@/auth/jwt/useJwt'
 import store              from '@/store'
 import administracion from './routes/administracion'
-import dashboard from './routes/dashboard'
 import uiElements from './routes/ui-elements/index'
-import pages from './routes/pages'
-import chartsMaps from './routes/charts-maps'
-import formsTable from './routes/forms-tables'
-import others from './routes/others'
-
-
 Vue.use(VueRouter)
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -25,14 +17,10 @@ const router = new VueRouter({
     return { x: 0, y: 0 }
   },
   routes: [
+    { path: '/', redirect: { name: 'home' } },
     ...routesDefault,
     ...administracion,
-    // ...dashboard,
-    // ...pages,
-    // ...chartsMaps,
-    // ...formsTable,
     ...uiElements,
-    // ...others,
     {
       path: '*',
       redirect: 'error-404',
@@ -44,18 +32,13 @@ router.beforeEach((to, _, next) => {
   const isLoggedIn = isUserLoggedIn()
   let validateUser = false;
 
-//   if(typeof to.meta.isOut != 'undefined' && isLoggedIn != null){
-//     return next({ name: 'home' })
-//   }
-
   if(isLoggedIn){
     useJwt
     .validateUser({ tk   : store.state.app.userData.token, })
     .then(response => {
-      console.log('response',response)
       validateUser = response.data.data;
+      console.log('response.data.data -> ', response.data.data)
       store.commit('app/UPDATE_USERVALIDATION', response.data.data)
-      console.log('store.state.app.userValidation',store.state.app.userValidation)
     })
     .catch(error => {
       console.log(error);
